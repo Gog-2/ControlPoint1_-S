fffa178c564d902bf8fc1b1f5c34bee39efba1e6
Gog_2 <gog100500@gmail.com>
2025-09-13 11:18:54 +0300

    Add project files.

---
+++ ControlPoint/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace ControlPoint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a, b, c, _switch;
+            while (true)
+            {
+                Console.WriteLine("Это вариант 15");
+                Console.Write("Выберете задание от 1 до 3: ");
+                _switch = Convert.ToInt32(Console.ReadLine());
+                switch (_switch)
+                {
+                    case 1:
+                        Console.WriteLine("Число a:");
+                        a = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Число b:");
+                        b = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Число c:");
+                        c = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Сумма: " + (a + b + c) + " Произведение: " + (a * b * c));
+                        Console.WriteLine("");
+                        Console.WriteLine("");
+                        break;
+                    case 2:
+                        Console.WriteLine("Скорость в км/ч:");
+                        a = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Скорость часов(целых):");
+                        b = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Проехал: " + a * b);
+                        Console.WriteLine("");
+                        Console.WriteLine("");
+                        break;
+                    case 3:
+                        Console.WriteLine("Сторона квадрата: ");
+                        a = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("В каком типе измерения?");
+                        string type = Convert.ToString(Console.ReadLine());
+                        Console.WriteLine("Площадь куба: " + a * a + " " + type + " квадратных");
+                        Console.WriteLine("");
+                        Console.WriteLine("");
+                        break;
+                    default:
+                        Console.WriteLine("Не коректный выбор, попробуйте ещё раз");
+                        Console.WriteLine("");
+                        Console.WriteLine("");
+                        break;
+                }
+            }
+        }
+    }
+}
